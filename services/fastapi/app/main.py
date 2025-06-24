b1d3a856275c3ded7c8d4548a9152caef5e996a5
unknown <lmunn64@iastate.edu>
2025-06-24 13:49:40 -0500

    creating phony image jsons based on ids of phony properties and api ffor getting image objects

--- services/fastapi/app/main.py
+++ services/fastapi/app/main.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 from enum import IntEnum
 from pydantic import BaseModel, Field, ValidationError
 from fastapi import FastAPI, HTTPException
@@ -12,9 +12,7 @@ app = FastAPI()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 test_json_path = os.path.join(BASE_DIR, 'test_json', 'phony_properties.json')
 
-properties_cache = {
-    'all_properties': None
-}
+
 # model for the address of a listing
 class Address(BaseModel):
     city: str
@@ -47,11 +45,21 @@ class Property(BaseModel):
     capacity: Capacity
     house_rules: HouseRules
 
+class Image(BaseModel):
+    url: str
+    thumbnail_url: str
+    caption: str
+    order: int
+
+properties_cache = {
+    'all_properties': None,
+    'property_images' : Dict[str, List[Image]]
+}
 
 # NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -59,6 +67,7 @@ app.add_middleware(
 
 
 # When receiving actual api response, make "async def"
+# GET request to get all properties as List[Property]
 @app.get('/properties', response_model=List[Property])
 def get_properties():
     if properties_cache.get('all_properties'):
@@ -70,7 +79,6 @@ def get_properties():
         return {"error": "File is empty"}
     with open(test_json_path) as file:
         content = json.load(file)
-    print(content.get('data'))
     try:
         properties = [Property(
             id = item.get("id"),
@@ -87,3 +95,27 @@ def get_properties():
     except ValidationError as e:
         raise HTTPException(status_code = 409, detail = 'Validation error: External API has returned unexpected response format')
     return properties
+
+@app.get('/properties/{uuid}/images', response_model=List[Image])
+def get_images(uuid: str):
+    if uuid in properties_cache.get('property_images', {}):
+        print("returning cached image properties")
+        return properties_cache.get('property_images')[uuid]
+    json_file = uuid + '.json'
+    image_path = os.path.join(BASE_DIR, 'test_json', 'phony_images', json_file)
+    if not os.path.exists(image_path):
+        return {'error' : "File not found"}
+    with open(image_path) as file:
+        content = json.load(file)
+
+    try:
+        images = [Image(
+            url = item.get('url'),
+            thumbnail_url= item.get('thumbnail_url'),
+            caption= item.get('caption'),
+            order= item.get('order')
+        ) for item in content.get('data')]
+    except ValidationError as e:
+        raise HTTPException(status_code=409, detail ='Validation error: External API has returned unexpected response format')
+    return images
+
