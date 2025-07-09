3de46fc1266201a9d7a40f1bc1b8f945ceab23de
unknown <lmunn64@iastate.edu>
2025-07-08 21:55:08 -0500

    fixing nuxt config for github pages static gen

--- services/nuxt-frontend/nuxt.config.ts
+++ services/nuxt-frontend/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  ssr: true,
+  nitro : {
+      preset: 'static'
+  },
+  app : {
+    baseURL: '/Jayph/'
+  },
   devtools: { enabled: true },
     modules: [
     '@pinia/nuxt',
