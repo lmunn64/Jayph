64588f6182ac8b77993a3a143022340433959196
unknown <lmunn64@iastate.edu>
2025-07-31 21:04:45 -0500

    removed google api key

--- services/nuxt-frontend/components/GMap.vue
+++ services/nuxt-frontend/components/GMap.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
     import { ref } from 'vue'
     import {GoogleMap} from 'vue3-google-map'
+    import.meta.env.GOOGLE_API_KEY
     const center = {lat: 41.4318592, lng: -87.9886336}
     const zoom = 13
 </script>
@@ -9,7 +10,7 @@
 <template>
 
     <GoogleMap class = "prop-map"
-    api-key="AIzaSyB_hQCpHH6Rj9V7grVWslNTMBHY3LxdvnU"
+    api-key="PEE"
     :center = "center"
     :zoom = "zoom"
     />
