31cb5287754d9fb0a01a3c9a55c4e0c4a9c1b741
unknown <lmunn64@iastate.edu>
2025-07-31 19:41:52 -0500

    added origin for github pages to get calendar data

--- services/fastapi/app/main.py
+++ services/fastapi/app/main.py
@@ -14,7 +14,7 @@ handler = Mangum(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://lmunn64.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
