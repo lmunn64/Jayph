3ef0944ba556324e5b19d5f9dba4e550c5d17e5a
unknown <lmunn64@iastate.edu>
2025-07-31 20:43:32 -0500

    added google maps basic

---
+++ services/nuxt-frontend/components/GMap.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+    import {GoogleMap} from 'vue3-google-map'
+    const center = {lat: 41.4318592, lng: -87.9886336}
+    const zoom = 13
+</script>
+
+
+<template>
+
+    <GoogleMap class = "prop-map"
+    api-key="AIzaSyB_hQCpHH6Rj9V7grVWslNTMBHY3LxdvnU"
+    :center = "center"
+    :zoom = "zoom"
+    />
+
+
+
+</template>
+
+<style>
+
+.prop-map{
+    width: 100%; 
+    height: 100%
+}
+</style>
--- services/nuxt-frontend/components/PropLocation.vue
+++ services/nuxt-frontend/components/PropLocation.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
+import GMap from './GMap.vue';
+
 
 </script>
 
 <template>
     <div class="location-wrapper">
-        <h1 style="text-align: center;"> "Find Us" Placeholder </h1>
+        <div class = "map-wrapper">
+            <GMap/>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.map-wrapper{  
+    display: flex;
+    justify-self: center;
+    align-self: center;
+    align-self: start;
+    width: 90%; 
+    height: 80%;
+}
 .location-wrapper {
     height: 500px;
     background-color: var(--accent-color);
